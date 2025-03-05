07f937312b26a87fb16f4cba2b3d9acf349f47ea
Julian Guban <jpguban04@gmail.com>
2025-03-06 02:16:29 +0800

    hehe

--- src/Parts/footer.js
+++ src/Parts/footer.js
@@ -2,16 +2,9 @@ import React from "react";
 
 function footer() {
   return (
-    <div
-      className="full-wdith flex-center flex-column"
-      style={{
-        padding: "15px",
-        margin: "10px",
-        borderTop: "3px solid #515864",
-      }}
-    >
+    <div className="footer">
       &copy; TUP-TAGUIG S.Y. 2022 - BTVTEICT-CP-3D
-      <p style={{padding: "10px"}}>PED5-Technology Teaching and Learning 2</p>
+      <p style={{ padding: "10px" }}>PED5-Technology Teaching and Learning 2</p>
     </div>
   );
 }
