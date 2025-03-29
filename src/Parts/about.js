b206736004eb05e7a307cef01754b9350ac9f337
Julian Guban <jpguban04@gmail.com>
2025-03-29 17:52:34 +0800

    abt

--- src/Parts/about.js
+++ src/Parts/about.js
@@ -10,9 +10,8 @@ function about() {
           <h1 className="about-title">ABOUT THE AUTHORS</h1>
         </div>
         <p style={{ width: "70%", textAlign: "center", padding: "40px 25px" }}>
-          We, The BTVTEICT-COMPUTER-SCIENCE-DEPARTMENT, are a group of students
-          who are passionate about technology and programming. We are dedicated
-          to providing you with the best learning experience possible. We hope
+          We are the Batch of S.Y. 2022 of BTVTEICT - Major in Computer Programming, 3rd Year students of TUP-TAGUIG. 
+          We are dedicated to provide you with the best learning experience possible. We hope
           you enjoy our content and find it helpful. If you have any questions
           or suggestions, please feel free to contact us. We would love to hear
           from you. Thank you for visiting our website!
