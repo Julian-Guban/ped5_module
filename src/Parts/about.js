e5fab8ea68f9a36aefbe229ee90052ed79f57e0a
Julian Guban <jpguban04@gmail.com>
2025-03-29 17:45:18 +0800

    footer batch

--- src/Parts/about.js
+++ src/Parts/about.js
@@ -10,11 +10,12 @@ function about() {
           <h1 className="about-title">ABOUT THE AUTHORS</h1>
         </div>
         <p style={{ width: "70%", textAlign: "center", padding: "40px 25px" }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-          tristique, purus nec convallis feugiat, velit purus malesuada urna, a
-          vehicula odio nunc sed nisl. Praesent ac lacus vitae mauris bibendum
-          tempus. Sed ut lorem et mi faucibus sollicitudin. Integer tincidunt
-          dictum leo, ac vehicula urna dapibus non. Vestibulum ut mauris leo.{" "}
+          We, The BTVTEICT-COMPUTER-SCIENCE-DEPARTMENT, are a group of students
+          who are passionate about technology and programming. We are dedicated
+          to providing you with the best learning experience possible. We hope
+          you enjoy our content and find it helpful. If you have any questions
+          or suggestions, please feel free to contact us. We would love to hear
+          from you. Thank you for visiting our website!
         </p>
       </div>
     </div>
--- src/Parts/footer.js
+++ src/Parts/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function footer() {
   return (
     <div className="footer">
-      &copy; TUP-TAGUIG S.Y. 2022 - BTVTEICT-CP-3D
+      &copy; TUP-TAGUIG Batch S.Y. 2022 - BTVTEICT-CP-3D
       <p style={{ padding: "10px" }}>PED5-Technology Teaching and Learning 2</p>
     </div>
   );
