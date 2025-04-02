353bc310f4dcc852ae3dec49889be60f69f282d2
Julian Guban <jpguban04@gmail.com>
2025-04-02 16:12:25 +0800

    chap2

--- src/Parts/topics.js
+++ src/Parts/topics.js
@@ -7,7 +7,7 @@ function topics() {
     <div className="full-width flex-center">
       <div className="cabinet">
        <CardTopics title={"Use Hand Tools"} num={1} link={"https://groupp3.vercel.app"}/>
-       <CardTopics title={"Install and Configure"} num={2}/>
+       <CardTopics title={"Install and Configure"} num={2} link={"https://chapter2-module.vercel.app"}/>
        <CardTopics title={"Set-up Computer Networks"} num={3}/>
        <CardTopics title={"Set-up Computer Servers"} num={4} link={"https://chapter4-module.vercel.app/"}/>
        <CardTopics title={"Maintain and Repair Computer Systems and Networks"} num={5} link={"https://chapter5-cyan.vercel.app/"}/>
